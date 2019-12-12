93209ff0e9841886a5ea26a1eeb8a04178728591
Stephen Fox <stephen@capablecreative.com>
2019-12-11 20:59:04 -0600

    Update to styling - SLF

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Header = ({ restartGame }) => (
   <div className="grid-header-container">
+    <h1>The Fantastic <span>Clicky-Match Game</span></h1>
     <div className="justify-left timer"></div>
     <div className="justify-center game-status-text"></div>
-    <div className="justify-end">
+    <div className="">
       <button onClick={restartGame} className="restart-button">Restart Game</button>
     </div>
   </div>
